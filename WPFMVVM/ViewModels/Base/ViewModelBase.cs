efdba611a92eef3ee3ae03d8660af6921b9ff687
Flazer FAM <nicholas620@mail.ru>
2023-09-03 20:11:27 +0400

    45 33

--- WPFMVVM/ViewModels/Base/ViewModelBase.cs
+++ WPFMVVM/ViewModels/Base/ViewModelBase.cs
@@ -1,12 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Documents;
 
 namespace WPFMVVM.ViewModels.Base
 {
-    internal class ViewModelBase
+    internal abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T feild, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(feild, value)) return false;
+            feild = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        private bool _Disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if(!disposing || _Disposed) return;
+            _Disposed = true;
+        }
     }
 }
---
+++ WPFMVVM/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFMVVM.ViewModels.Base;
+
+namespace WPFMVVM.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Заголовок окна
+        private string _Title = "TestW";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+    }
+}
